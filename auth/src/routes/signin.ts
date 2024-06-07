bf172f68ab06fee3bc5648b77d0400b92de3aa2b
Matthias Mayrock <matthias.mayrock@globalchanger.com>
2024-06-07 19:29:54 +0200

    Add validation middle ware

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,44 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import { body } from "express-validator";
+import { User } from "../models/user";
+import { BadRequestError } from "../errors/bad-request-error";
+import { validateRequest } from "../middlewares/validate-request";
 
 const router = express.Router();
 
-router.post("/api/users/signin", (req, res) => {
+router.get("/api/users/signin/hello", (req, res) => {
   res.send("Hi there!");
 });
+
+router.post(
+  "/api/users/signin",
+  [
+    body("email").isEmail().withMessage("Email must be valid"),
+    body("password").trim().notEmpty().withMessage("Password required"),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) {
+      throw new BadRequestError("User not found");
+    }
+
+    // const user = User.build({ email, password });
+    // await user.save();
+
+    // const userJwt = jwt.sign(
+    //   {
+    //     id: user.id,
+    //     email: user.email,
+    //   },
+    //   process.env.JWT_KEY!
+    // );
+
+    // req.session = { jwt: userJwt };
+    res.status(201).send({});
+  }
+);
 
 export { router as signinRouter };
