b3c3ec66b35adf98a79e65cfe321796673f30497
Matthias Mayrock <matthias.mayrock@globalchanger.com>
2024-06-09 16:54:19 +0200

    Evaluate current user

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -4,9 +4,12 @@ const Page = async () => {
   const client = buildClient();
   const { data } = await client.get('/api/users/currentuser');
 
-  console.log(data)
-
-  return <h1>Page</h1>
+  if (data.currentUser) {
+    return <h1>You are signed in</h1>
+  }
+  else {
+    return <h1>You are not signed</h1>
+  }
 };
  
 export default Page;
