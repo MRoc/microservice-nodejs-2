29847dd84f769b2e99aad4c05881e404c5c43cc9
Matthias Mayrock <matthias.mayrock@globalchanger.com>
2024-06-10 20:34:19 +0200

    Pass connection string from deployment file through env

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -4,11 +4,15 @@ import { app } from "./app";
 const start = async () => {
   try {
     if (!process.env.JWT_KEY) {
-      throw new Error("JWT not found");
+      throw new Error("JWT_KEY not found");
+    }
+    if (!process.env.MONGO_URI) {
+      throw new Error("MONGO_URI not found");
     }
 
-    await mongoose.connect("mongodb://tickets-mongo-srv:27017/tickets");
-    console.log("Connected to MongoDb");
+    console.log(`Connecting to MongoDb '${process.env.MONGO_URI}'...`);
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Connected to MongoDb!");
 
     app.listen(3000, () => {
       console.log("Tickets service is listening on port 3000");
