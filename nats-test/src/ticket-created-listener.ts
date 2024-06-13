67167d53248461645280b1dcf7ca7025b04c0ac3
Matthias Mayrock <mail@mroc.de>
2024-06-13 15:04:38 +0200

    Typified listener

--- nats-test/src/ticket-created-listener.ts
+++ nats-test/src/ticket-created-listener.ts
@@ -1,16 +1,20 @@
 import { Message, Stan } from "node-nats-streaming";
 import { BaseListener } from "./base-listener";
+import { TicketCreatedEvent } from "./ticket-created-event";
+import { Subjects } from "./subjects";
 
-export class TicketCreatedListener extends BaseListener {
-  subject = "ticket:created";
+export class TicketCreatedListener extends BaseListener<TicketCreatedEvent> {
+  readonly subject = Subjects.TicketCreated;
   queueGroupName = "payments-service";
 
   constructor(client: Stan) {
     super(client);
   }
 
-  onMessage(data: any, msg: Message) {
-    console.log("Event data!", data);
+  onMessage(data: TicketCreatedEvent["data"], msg: Message) {
+    console.log("+-- Event data!", data);
+    console.log(`+-- ${data.title}`);
+    console.log(`+-- ${data.price}`);
     msg.ack();
   }
 }
