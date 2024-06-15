98abbbb382c3feced0537066216fb0c87501e8e2
Matthias Mayrock <matthias.mayrock@globalchanger.com>
2024-06-15 15:37:16 +0200

    Add delete endpoint

---
+++ orders/src/routes/__test__/delete.test.ts
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+import request from "supertest";
+import { app } from "../../app";
+import { Ticket } from "../../models/ticket";
+import { OrderStatus } from "@mroc/ex-ms-common/build";
+import { Order } from "../../models/order";
+
+it("return 401 if user is not signed in", async () => {
+  const orderId = new mongoose.Types.ObjectId().toHexString();
+  await request(app).delete(`/api/orders/${orderId}`).send().expect(401);
+});
+
+it("returns 404 if orderId is invalid", async () => {
+  const orderId = new mongoose.Types.ObjectId().toHexString();
+  await request(app)
+    .delete(`/api/orders/${orderId}`)
+    .set("Cookie", signin())
+    .send()
+    .expect(404);
+});
+
+it("cancels the order", async () => {
+  const ticket = await Ticket.build({ title: "Title", price: 20 });
+  await ticket.save();
+
+  const user = signin();
+
+  const { body: order1 } = await request(app)
+    .post("/api/orders")
+    .set("Cookie", user)
+    .send({ ticketId: ticket.id })
+    .expect(201);
+
+  await request(app)
+    .delete(`/api/orders/${order1.id}`)
+    .set("Cookie", user)
+    .send()
+    .expect(204);
+
+  const updatedOrder = await Order.findById(order1.id);
+  expect(updatedOrder!.status).toBe(OrderStatus.Cancelled);
+});
+
+it.todo("emits an order cancelled event");
--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,9 +1,35 @@
+import {
+  NotAuthorizedError,
+  NotFoundError,
+  OrderStatus,
+  requireAuth,
+} from "@mroc/ex-ms-common/build";
 import express, { Request, Response } from "express";
+import { Order } from "../models/order";
 
 const router = express.Router();
 
-router.delete("/api/orders/:oderId", async (req: Request, res: Response) => {
-  res.send({});
-});
+router.delete(
+  "/api/orders/:orderId",
+  requireAuth,
+  async (req: Request, res: Response) => {
+    const order = await Order.findById(req.params.orderId).populate("ticket");
+
+    if (!order) {
+      throw new NotFoundError();
+    }
+
+    if (order.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+
+    order.status = OrderStatus.Cancelled;
+    await order.save();
+
+    // Needs to publish an event!
+
+    return res.status(204).send(order);
+  }
+);
 
 export { router as deleteOrderRouter };
