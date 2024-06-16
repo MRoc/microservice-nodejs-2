99b356943d326e818bff5b6e44af28d7a6b69afc
Matthias Mayrock <matthias.mayrock@globalchanger.com>
2024-06-16 18:49:12 +0200

    Update test for listener

--- orders/src/events/listeners/__test__/ticket-updated-listener.test.ts
+++ orders/src/events/listeners/__test__/ticket-updated-listener.test.ts
@@ -1,18 +1,27 @@
-import { TicketCreatedEvent, natsWrapper } from "@mroc/ex-ms-common/build";
-import { TicketCreatedListener } from "../ticket-created-listener";
+import { TicketUpdatedEvent, natsWrapper } from "@mroc/ex-ms-common/build";
+import { TicketUpdatedListener } from "../ticket-updated-listener";
 import mongoose from "mongoose";
 import { Message } from "node-nats-streaming";
 import { Ticket } from "../../../models/ticket";
 
 const setup = async () => {
-  const listener = new TicketCreatedListener(natsWrapper.client());
-  const data: TicketCreatedEvent["data"] = {
+  const listener = new TicketUpdatedListener(natsWrapper.client());
+
+  const ticket = Ticket.build({
     id: new mongoose.Types.ObjectId().toHexString(),
-    version: 0,
     title: "Title",
     price: 10,
+  });
+  await ticket.save();
+
+  const data: TicketUpdatedEvent["data"] = {
+    id: ticket.id,
+    version: ticket.version + 1,
+    title: "Title",
+    price: 20,
     userId: new mongoose.Types.ObjectId().toHexString(),
   };
+
   // @ts-ignore
   const msg: Message = {
     ack: jest.fn(),
@@ -21,11 +30,12 @@ const setup = async () => {
   return { listener, data, msg };
 };
 
-it("creates and saves a ticket", async () => {
+it("finds and updates a ticket", async () => {
   const { listener, data, msg } = await setup();
   await listener.onMessage(data, msg);
   const ticket = await Ticket.findById(data.id);
-  expect(ticket).toBeDefined();
+  expect(ticket!.price).toEqual(20);
+  expect(ticket!.version).toEqual(1);
 });
 
 it("acks the message", async () => {
