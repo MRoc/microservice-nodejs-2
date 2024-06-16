ac9017c0e6b07e3e7b7ae16c4029a5796d6f8378
Matthias Mayrock <matthias.mayrock@globalchanger.com>
2024-06-16 16:41:00 +0200

    Add tests

---
+++ orders/src/events/listeners/__test__/ticket-created-listener.test.ts
@@ -0,0 +1,39 @@
+import {
+  Subjects,
+  TicketCreatedEvent,
+  natsWrapper,
+} from "@mroc/ex-ms-common/build";
+import { TicketCreatedListener } from "../ticket-created-listener";
+import mongoose from "mongoose";
+import { Message } from "node-nats-streaming";
+import { Ticket } from "../../../models/ticket";
+
+const setup = async () => {
+  const listener = new TicketCreatedListener(natsWrapper.client());
+  const data: TicketCreatedEvent["data"] = {
+    id: new mongoose.Types.ObjectId().toHexString(),
+    version: 0,
+    title: "Title",
+    price: 10,
+    userId: new mongoose.Types.ObjectId().toHexString(),
+  };
+  // @ts-ignore
+  const msg: Message = {
+    ack: jest.fn(),
+  };
+
+  return { listener, data, msg };
+};
+
+it("creates and saves a ticket", async () => {
+  const { listener, data, msg } = await setup();
+  await listener.onMessage(data, msg);
+  const ticket = await Ticket.findById(data.id);
+  expect(ticket).toBeDefined();
+});
+
+it("acks the message", async () => {
+  const { listener, data, msg } = await setup();
+  await listener.onMessage(data, msg);
+  expect(msg.ack).toHaveBeenCalled();
+});
