93b90365f80d60cec3a7ffcac19d257e47eef70c
Matthias Mayrock <mail@mroc.de>
2024-06-21 14:03:42 +0200

    First service

--- expiration/src/index.ts
+++ expiration/src/index.ts
@@ -1,34 +1,17 @@
 import { natsWrapper } from "@mroc/ex-ms-common";
 import { OrderCreatedListener } from "./events/listeners/order-created-listener";
+import {
+  throwIfMissingMongoConfig,
+  throwIfMissingNatsConfig,
+  connectNsts,
+} from "@mroc/ex-ms-common";
 
 const start = async () => {
   try {
-    if (!process.env.NATS_URI) {
-      throw new Error("NATS_URI not found");
-    }
-    if (!process.env.NATS_CLUSTER_ID) {
-      throw new Error("NATS_CLUSTER_ID not found");
-    }
-    if (!process.env.NATS_CLIENT_ID) {
-      throw new Error("NATS_CLIENT_ID not found");
-    }
+    throwIfMissingMongoConfig();
+    throwIfMissingNatsConfig();
 
-    console.log(
-      `Connecting to NATS '${process.env.NATS_URI}' cluster '${process.env.NATS_CLUSTER_ID}' with ID '${process.env.NATS_CLIENT_ID}'...`
-    );
-    await natsWrapper.connect(
-      process.env.NATS_CLUSTER_ID,
-      process.env.NATS_CLIENT_ID,
-      process.env.NATS_URI
-    );
-    console.log("Connected to NATS!");
-
-    natsWrapper.client().on("close", () => {
-      console.log(`NATS connection closed!`);
-      process.exit();
-    });
-    process.on("SIGINT", () => natsWrapper.client().close());
-    process.on("SIGTERM", () => natsWrapper.client().close());
+    connectNsts();
 
     console.log("Expiration service is starting...");
     new OrderCreatedListener(natsWrapper.client()).listen();
