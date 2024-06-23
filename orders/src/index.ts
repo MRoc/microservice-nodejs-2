6ea2f241da048b5fa61bd285e313679114b31d3e
Matthias Mayrock <matthias.mayrock@globalchanger.com>
2024-06-23 16:16:09 +0200

    Use connector in order service

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -1,6 +1,12 @@
-import mongoose from "mongoose";
+import mongoose, { connect } from "mongoose";
 import { app } from "./app";
-import { natsWrapper } from "@mroc/ex-ms-common";
+import {
+  natsWrapper,
+  throwIfMissingNatsConfig,
+  throwIfMissingMongoConfig,
+  connectMongo,
+  connectNsts,
+} from "@mroc/ex-ms-common";
 import { TicketCreatedListener } from "./events/listeners/ticket-created-listener";
 import { TicketUpdatedListener } from "./events/listeners/ticket-updated-listener";
 import { ExpirationCompleteListener } from "./events/listeners/expiration-complete-listener";
@@ -12,45 +18,17 @@ const start = async () => {
       throw new Error("JWT_KEY not found");
     }
 
-    if (!process.env.MONGO_URI) {
-      throw new Error("MONGO_URI not found");
-    }
+    throwIfMissingNatsConfig();
+    throwIfMissingMongoConfig();
 
-    if (!process.env.NATS_URI) {
-      throw new Error("NATS_URI not found");
-    }
-    if (!process.env.NATS_CLUSTER_ID) {
-      throw new Error("NATS_CLUSTER_ID not found");
-    }
-    if (!process.env.NATS_CLIENT_ID) {
-      throw new Error("NATS_CLIENT_ID not found");
-    }
-
-    console.log(
-      `Connecting to NATS '${process.env.NATS_URI}' cluster '${process.env.NATS_CLUSTER_ID}' with ID '${process.env.NATS_CLIENT_ID}'...`
-    );
-    await natsWrapper.connect(
-      process.env.NATS_CLUSTER_ID,
-      process.env.NATS_CLIENT_ID,
-      process.env.NATS_URI
-    );
-    console.log("Connected to NATS!");
-
-    natsWrapper.client().on("close", () => {
-      console.log(`NATS connection closed!`);
-      process.exit();
-    });
-    process.on("SIGINT", () => natsWrapper.client().close());
-    process.on("SIGTERM", () => natsWrapper.client().close());
+    connectNsts();
 
     new TicketCreatedListener(natsWrapper.client()).listen();
     new TicketUpdatedListener(natsWrapper.client()).listen();
     new ExpirationCompleteListener(natsWrapper.client()).listen();
     new PaymentCreatedListener(natsWrapper.client()).listen();
 
-    console.log(`Connecting to MongoDb '${process.env.MONGO_URI}'...`);
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("Connected to MongoDb!");
+    connectMongo();
 
     app.listen(3000, () => {
       console.log("Orders service is listening on port 3000");
