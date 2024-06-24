3f40b5ea6eebef960dfcd5a933570ddc2dd1bad5
Matthias Mayrock <mail@mroc.de>
2024-06-24 20:59:34 +0200

    Use common

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,18 +1,14 @@
-import mongoose from "mongoose";
 import { app } from "./app";
+import { throwIfMissingMongoConfig, connectMongo } from "@mroc/ex-ms-common";
 
 const start = async () => {
   try {
     if (!process.env.JWT_KEY) {
       throw new Error("JWT_KEY not found");
     }
-    if (!process.env.MONGO_URI) {
-      throw new Error("MONGO_URI not found");
-    }
 
-    console.log(`Connecting to MongoDb '${process.env.MONGO_URI}'...`);
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("Connected to MongoDb!");
+    throwIfMissingMongoConfig();
+    connectMongo();
 
     app.listen(3000, () => {
       console.log("Auth service is listening on port 3000");
