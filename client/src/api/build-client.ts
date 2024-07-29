d6727c239ba086f757a2f9100b78cf7544e89ea2
Matthias Mayrock <mail@mroc.de>
2024-07-29 12:56:00 +0200

    Move cluster base URL to env

--- client/src/api/build-client.ts
+++ client/src/api/build-client.ts
@@ -4,9 +4,13 @@ const buildClient = async (): Promise<AxiosInstance> => {
   if (typeof window === "undefined") {
     console.log(`Running buildClient on the server!`);
     const { headers } = await import("next/headers");
+
+    if (!process.env.CLUSTER_BASE_URL) {
+      throw new Error("CLUSTER_BASE_URL is not defined!");
+    }
+    
     return axios.create({
-      baseURL: "http://seqo.io/",
-      // "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local",
+      baseURL: process.env.CLUSTER_BASE_URL,
       headers: {
         Host: headers().get("Host"),
         Cookie: headers().get("Cookie"),
